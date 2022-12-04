5cba10cac19f51f3b5a1f65fc213afa3e5b83186
Rainer Jung <r@iner.cc>
2022-12-04 07:28:01 +0100

    Added some explanation of 2022/04

--- 2022/04.rb
+++ 2022/04.rb
@@ -1,9 +1,20 @@
 #!/usr/bin/env ruby
 
-# [[["2", "3", "4"], ["6", "7", "8"]], [["2", "3"], ["4", "5"]], …]
-input = (ARGV.empty? ? DATA : ARGF).read
-  .split(/\n/)
-  .map { |a| a.split(/,/).map { |b| b.split(/-/).map(&:to_i) }.map { |d| (d[0]..d[1]).to_a } }
+# The trick I use is to not just use the values, but to retrieve all the
+# sections from the puzzle input. Then the `&` operator on
+# [Array](https://ruby-doc.org/current/Array.html) returns all intersecting
+# sections of the two arrays.
+#
+# For the first solution, I check, if the result of all entries of both is the
+# same as one of the two arrays.  
+# For the 2nd solution, I just check, if there's any intersection.
+
+# [[[2, 3, 4], [6, 7, 8]], [[2, 3], [4, 5]], [[5, 6, 7], [7, 8, 9]], …]
+input = (ARGV.empty? ? DATA : ARGF).readlines
+  .map { |a| a.split(/,/)                      # Separate Elves
+              .map { |b| b.split(/-/)          # Retrieve range borders
+                          .map(&:to_i) }
+              .map { |d| (d[0]..d[1]).to_a } } # Convert to range and array
 
 solution1 = input.filter { |a, b| [a, b].include?(a & b) }.size
 solution2 = input.filter { |a, b| (a & b).any? }.size
